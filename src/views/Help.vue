5628e6687cae3b898e774504bc0e5f1905e1fc82
Ty Cooper <omi95ty@gmail.com>
2019-10-06 10:15:08 -0400

    Minor Updates #1
    10/6/2019

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -3,6 +3,7 @@
     <h1>This is the Help page</h1>
     <hr />
     <p>Read all about our web3 application below!</p>
+    <p><small>More infomration coming soon</small></p>
   </div>
 </template>
 <script>
